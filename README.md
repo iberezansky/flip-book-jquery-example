9cb569e15258939b68d38ec426e256922502b90c
iberezansky <ivberezansky@gmail.com>
2017-03-09 13:21:59 +0300

    Initial.

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1>3D FlipBook jQuery plugin example</h1>
+
+<p>
+  This is an example for <a href="https://www.npmjs.com/package/flip-book">flip-book</a> npm package. It is recommended to see <a href="http://3dflipbook.iberezansky.net/">the live preview</a>.
+</p>
+
+<h2>How to use</h2>
+
+Clone a copy of the main git repo by running:
+```bash
+git clone https://github.com/iberezansky/flip-book-jquery-example.git
+```
+
+Enter the flip-book-jquery-example directory and run the installation:
+```bash
+cd flip-book-jquery-example && npm install
+```
+
+When the installation finished run <a href="http://localhost:8080/">development server</a>:
+```bash
+npm run dev
+```
+
+To build debug version:
+
+```bash
+npm run build-debug
+```
+
+To build production version:
+```bash
+npm run build-production
+```
+
+<p>
+  For details see <a href="http://3dflipbook.iberezansky.net/documentation">the documentation</a>.
+</p>
---
+++ index.js
@@ -0,0 +1,108 @@
+import {$} from './src/libs'
+
+window.PDFJS_LOCALE = {
+  pdfJsWorker: 'node_modules/flip-book/js/pdf.worker.js'
+};
+require('flip-book');
+
+
+// // Sample 0 {
+// $('#container').FlipBook({
+//   pdf: 'books/pdf/FoxitPdfSdk.pdf',
+//   template: {
+//     html: 'node_modules/flip-book/templates/default-book-view.html',
+//     styles: [
+//       'node_modules/flip-book/css/short-black-book-view.css'
+//     ],
+//     script: 'node_modules/flip-book/js/default-book-view.js'
+//   }
+// });
+// // }
+
+// Sample 1 {
+function theKingIsBlackPageCallback(n) {
+  return {
+    type: 'image',
+    src: 'books/image/theKingIsBlack/'+(n+1)+'.jpg',
+    interactive: false
+  };
+}
+
+$('#container').FlipBook({
+  pageCallback: theKingIsBlackPageCallback,
+  pages: 10,
+  propertiesCallback: function(props) {
+    props.cover.color = 0x000000;
+    return props;
+  },
+  template: {
+    html: 'node_modules/flip-book/templates/default-book-view.html',
+    styles: [
+      'node_modules/flip-book/css/short-white-book-view.css'
+    ],
+    script: 'node_modules/flip-book/js/default-book-view.js',
+    sounds: {
+      startFlip: 'node_modules/flip-book/sounds/start-flip.mp3',
+      endFlip: 'node_modules/flip-book/sounds/end-flip.mp3'
+    }
+  }
+});
+// }
+
+// // Sample 2 {
+// $('#container').FlipBook({
+//   pdf: 'books/pdf/CondoLiving.pdf',
+//   template: {
+//     html: 'node_modules/flip-book/templates/default-book-view.html',
+//     styles: [
+//       'node_modules/flip-book/css/white-book-view.css'
+//     ],
+//     script: 'node_modules/flip-book/js/default-book-view.js'
+//   }
+// });
+// // }
+//
+// // Sample 3 {
+// $('#container').FlipBook({
+//   pdf: 'books/pdf/TheThreeMusketeers.pdf',
+//   propertiesCallback: function(props) {
+//     props.page.depth /= 2.5;
+//     props.cover.padding = 0.002;
+//     return props;
+//   },
+//   template: {
+//     html: 'node_modules/flip-book/templates/default-book-view.html',
+//     styles: [
+//       'node_modules/flip-book/css/short-black-book-view.css'
+//     ],
+//     script: 'node_modules/flip-book/js/default-book-view.js'
+//   }
+// });
+// // }
+//
+// // Sample 4 {
+// function preview(n) {
+//   return {
+//     type: 'html',
+//     src: 'books/html/preview/'+(n%3+1)+'.html',
+//     interactive: true
+//   };
+// }
+//
+// $('#container').FlipBook({
+//   pageCallback: preview,
+//   pages: 10,
+//   propertiesCallback: function(props) {
+//     props.sheet.color = 0x008080;
+//     props.cover.padding = 0.002;
+//     return props;
+//   },
+//   template: {
+//     html: 'node_modules/flip-book/templates/default-book-view.html',
+//     styles: [
+//       'node_modules/flip-book/css/black-book-view.css'
+//     ],
+//     script: 'node_modules/flip-book/js/default-book-view.js'
+//   }
+// });
+// // }
